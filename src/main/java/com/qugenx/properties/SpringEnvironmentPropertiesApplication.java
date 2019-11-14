10774ba3cf54badd04f260066ba6a6d30f732564
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-11-14 17:17:32 +0100

    first commit

---
+++ src/main/java/com/qugenx/properties/SpringEnvironmentPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.qugenx.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnvironmentPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEnvironmentPropertiesApplication.class, args);
+    }
+
+}
